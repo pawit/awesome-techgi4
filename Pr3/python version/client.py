ada13b28d965e1de12cec1a7632b001d22505009
Tom Nick <Tom.W.A.Nick@gmail.com>
2013-06-01 00:18:20 +0200

    python version works :), you need python3

--- Pr3/python version/client.py
+++ Pr3/python version/client.py
@@ -10,6 +10,19 @@ import sys
 from mosquitto import Mosquitto
 
 topic = 'techgi4/msgboard'
+debug = False
+
+
+def max_clocks(c1, c2):
+    new_clock = c1
+    for k1 in c1:
+        if k1 in c2 and c1[k1] >= c2[k1]:
+            new_clock[k1] = c1[k1]
+    for k2 in c2:
+        if k2 and c2[k2] > c1[k2]:
+            new_clock[k2] = c2[k2]
+    return new_clock
+
 
 class Client(Mosquitto):
     def __init__(self, host, port, client_id):
@@ -22,19 +35,49 @@ class Client(Mosquitto):
 
         self.connect_async(host, port)
 
+        # I don't know yet, where the id is stored
+        self.client_id = client_id
+        # we use a hash_array where the keys are the ids of the clients
+        self.vector_clock = {self.client_id: 0}
+        # queue to save messages
+        self.queue = []
+
     def _on_connect(self, mosq, userdata, rc):
         print("Connect : %s" % rc)
         self.subscribe(topic, 2)
-    
+
     def _on_subscribe(self, mosq, userdata, mid, granted_qos):
-        print("Subscribed: mid %s granted_qos %s" % (mid, granted_qos) )
+        print("Subscribed: mid %s granted_qos %s" % (mid, granted_qos))
 
     def _on_message(self, mosq, userdata, message):
-        # see class MosquittoMessage for message type
-        print("TODO message received callback")
+        # we don't increment when receiving!
+        # Is there anything missing?
+        m, i, c = eval(message.payload)
+        if i == self.client_id:
+            return
+        if i not in self.vector_clock:
+            self.vector_clock[i] = 0
+        if self.vector_clock[i] + 1 != c[i]:
+            # something fishy, but what is it?
+            self.queue.append(message)
+            if debug:
+                print('waiting for message...')
+        else:
+            print("%s: %s" % (i, m))
+            self.vector_clock = max_clocks(self.vector_clock, c)
+            # we received a message, are there any messages that follow this message?
+            # could use a sorted list
+            for me in self.queue:
+                for mes in self.queue:
+                    m, i, c = eval(mes.payload)
+                    if self.vector_clock[i] + 1 == c[i]:
+                        self.vector_clock = max_clocks(self.vector_clock, c)
+                        print("%s: %s" % (i, m))
+
 
     def on_stdin(self, message):
-        print("TODO stdin mesg : %s" % message)
+        self.vector_clock[self.client_id] += 1
+        self.publish(topic, str([message,self.client_id, self.vector_clock]), 0)
 
 if __name__ == '__main__':
     # register signal handler for strg-c
@@ -45,9 +88,9 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
 
     # default values
-    client_id = ''
-    host = 'localhost'
-    port = 9999
+    client_id = '0'
+    host = '127.0.0.1'
+    port = 1883
 
     if len(sys.argv) > 1:
         host = sys.argv[1]
@@ -63,7 +106,7 @@ if __name__ == '__main__':
 
     epoll = select.epoll()
     epoll.register(sys.stdin.fileno(), select.EPOLLIN | select.EPOLLET)
-    
+
     while True:
         events = epoll.poll()
         for fileno, event in events:
